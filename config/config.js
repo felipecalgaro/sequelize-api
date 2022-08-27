3f598a85c47673bf2e37a9947ca8a830c031e105
Felipe <pipecalgaro@gmail.com>
2022-08-27 18:32:42 -0300

    first commit

---
+++ config/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    dialect: 'postgres',
+    host: 'localhost',
+    username: 'postgres',
+    password: 'pipe260806',
+    database: 'curso_sequelize',
+    define: {
+        timestamps: true
+    }
+}
---
+++ controller/PlanetController.js
@@ -0,0 +1,30 @@
+const Planet = require('../models/Planet')
+
+module.exports = {
+  async store(req, res) {
+    const { name, position } = req.body
+
+    const planet = await Planet.create({ name, position })
+    return res.json(planet)
+  },
+
+  async index(req, res) {
+    const planets = await Planet.findAll()
+    return res.json(planets)
+  },
+
+  async put(req, res) {
+    const { name, size, position } = req.body
+    const { id } = req.params
+
+    await Planet.update({ name, size, position }, { where: { id } })
+    return res.send('Planet updated successfully')
+  },
+
+  async delete(req, res) {
+    const { id } = req.params
+
+    await Planet.destroy({ where: { id } })
+    return res.send('Planet deleted successfully')
+  }
+}
